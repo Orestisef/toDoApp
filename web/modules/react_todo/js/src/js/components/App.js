dbaff4da8795e20423d0e72a3a6593ec67588f41
orestisef <orestisef@gmail.com>
2020-06-10 00:16:18 +0300

    css color style

--- web/modules/react_todo/js/src/js/components/App.js
+++ web/modules/react_todo/js/src/js/components/App.js
@@ -103,7 +103,7 @@ const Header = (props) => {
   return(
     <div className='card-header'>
       <h1 className='card-header-title header'>
-        You have {props.numTodos} Todos
+        {props.numTodos} Todos
       </h1>
     </div>
   )
