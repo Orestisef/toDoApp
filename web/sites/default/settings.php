931858a64ff5dc36b53827efae19f77f991430a5
orestisef <orestisef@gmail.com>
2020-06-11 22:34:07 +0300

    upload db and set up installations steps

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -789,17 +789,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
+ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+   include $app_root . '/' . $site_path . '/settings.local.php';
+ }
 $settings['config_sync_directory'] = '../config/sync';
-$databases['default']['default'] = array (
-  'database' => 'toDoReact',
-  'username' => 'root',
-  'password' => 'root',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
